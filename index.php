bdae3f1ce57104516054b0f0aecb40abd005477b
VitorasTavares <vitorastavares@gmail.com>
2019-11-08 12:22:29 -0300

    projeto faceistagram

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+$rotas = key($_GET)?key($_GET):"posts";
+
+switch($rotas){
+    case "posts":
+        include "controllers/PostController.php";
+        $controller = new PostController();
+        $controller->acao($rotas);
+    break;
+
+    case "formulario-posts":
+    include "controllers/PostController.php";
+    $controller = new PostController();
+    $controller->acao($rotas);
+break;
+
+    case "cadastrar-post":
+    include "controllers/PostController.php";
+    $controller = new PostController();
+    $controller->acao($rotas);
+break;
+}
+
+
+
